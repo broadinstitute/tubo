7a00efdf73c14dbf7c66ce0c30a6e8e33cd43ce8
Allen Goodman <allen.goodman@icloud.com>
2020-04-21 15:13:04 -0400

    object detection

--- src/CameraScreen/CameraScreen.tsx
+++ src/CameraScreen/CameraScreen.tsx
@@ -1,5 +1,5 @@
-import React, {useEffect, useRef, useState} from "react";
-import {Camera, CameraCapturedPicture} from "expo-camera";
+import React, {useEffect, useState} from "react";
+import {Camera} from "expo-camera";
 import {Platform, Text, TouchableOpacity, View} from "react-native";
 import {askAsync, CAMERA} from "expo-permissions";
 import {RouteProp} from "@react-navigation/native";
@@ -7,43 +7,147 @@ import {ScreenStack} from "../App";
 import {StackNavigationProp} from "@react-navigation/stack";
 import * as tensorflow from '@tensorflow/tfjs';
 import {style} from "./CameraScreen.style";
-import { cameraWithTensors } from '@tensorflow/tfjs-react-native';
+import {cameraWithTensors} from '@tensorflow/tfjs-react-native';
+import {DetectedObject} from '@tensorflow-models/coco-ssd';
+import {CLASSES} from "@tensorflow-models/coco-ssd/dist/classes";
+import Svg, {G, Rect} from "react-native-svg";
 
 type CameraScreenProps = {
   navigation: StackNavigationProp<ScreenStack, 'Camera'>;
   route: RouteProp<ScreenStack, 'Camera'>;
 };
 
-const resource = "https://tfhub.dev/tensorflow/tfjs-model/ssd_mobilenet_v2/1/default/1";
+const ORIGIN = 'https://tfhub.dev/tensorflow/tfjs-model/ssd_mobilenet_v2/1/default/1';
 
 const TensorCamera = cameraWithTensors(Camera);
 
-export const CameraScreen = ({navigation}: CameraScreenProps) => {
-  const ref = useRef<Camera>(null);
+const config = {
 
-  const [graph, setGraph] = useState<tensorflow.GraphModel | null>();
+};
+
+const buildDetectedObjects = (width: number, height: number, boxes: Float32Array, scores: number[], indexes: Float32Array, classes: number[]): DetectedObject[] => {
+  const count = indexes.length;
+  const objects: DetectedObject[] = [];
+  for (let i = 0; i < count; i++) {
+    const bbox = [];
+    for (let j = 0; j < 4; j++) {
+      bbox[j] = boxes[indexes[i] * 4 + j];
+    }
+    const minY = bbox[0] * height;
+    const minX = bbox[1] * width;
+    const maxY = bbox[2] * height;
+    const maxX = bbox[3] * width;
+    bbox[0] = minX;
+    bbox[1] = minY;
+    bbox[2] = maxX - minX;
+    bbox[3] = maxY - minY;
+    objects.push({
+      bbox: bbox as [number, number, number, number],
+      class: CLASSES[classes[indexes[i]] + 1].displayName,
+      score: scores[indexes[i]]
+    });
+  }
+  return objects;
+};
+
+const calculateMaxScores = (scores: Float32Array, numBoxes: number, numClasses: number): [number[], number[]] => {
+  const maxes = [];
+  const classes = [];
+
+  for (let i = 0; i < numBoxes; i++) {
+    let max = Number.MIN_VALUE;
+    let index = -1;
+    for (let j = 0; j < numClasses; j++) {
+      if (scores[i * numClasses + j] > max) {
+        max = scores[i * numClasses + j];
+        index = j;
+      }
+    }
+    maxes[i] = max;
+    classes[i] = index;
+  }
+  return [maxes, classes];
+};
+
+export const CameraScreen = ({navigation}: CameraScreenProps) => {
+  const [detections, setDetections] = useState<DetectedObject[]>([]);
+  const [graph, setGraph] = useState<tensorflow.GraphModel | null>(null);
   const [images, setImages] = useState<IterableIterator<tensorflow.Tensor3D>>();
   const [permission, setPermission] = useState<boolean | null>(null);
-  const [pressed, setPressed] = useState<boolean>(false);
+  const [rectangles, setRectangles] = useState<any[]>([]);
+
+  useEffect(() => {
+    const rects = detections.map((detection: DetectedObject, index: number) => {
+      return <Rect
+        fill={'red'}
+        fillOpacity={0.2}
+        height={(detection.bbox[3] - detection.bbox[1])}
+        key={index}
+        width={(detection.bbox[2] - detection.bbox[0])}
+        x={detection.bbox[0]}
+        y={detection.bbox[1]}
+      />;
+    });
+
+    setRectangles(rects);
+  }, [detections]);
 
   useEffect(() => {
     const f = async () => {
       if (images) {
         const image = await images.next().value;
 
-        console.info(image);
-
         if (image) {
-          const inputs = {
-            image_tensor: tensorflow.expandDims(image, 0)
-          };
+          const x = image.expandDims();
 
-          const prediction = await graph?.executeAsync(inputs) as Array<tensorflow.Tensor>;
+          if (graph) {
+            const y = await graph.executeAsync(x) as tensorflow.Tensor[];
 
-          const categories = prediction[0];
-          const geometries = prediction[1];
+            // console.info("y: " + y);
 
-          console.info(categories);
+            if (y) {
+              const categories_shape: number[] = y[0].shape;
+              const geometries_shape: number[] = y[1].shape;
+
+              const categories = y[0].dataSync() as Float32Array;
+              const geometries = y[1].dataSync() as Float32Array;
+
+              x.dispose();
+
+              tensorflow.dispose(y);
+
+              // console.info("categories: " + categories);
+              // console.info("geometries: " + geometries);
+
+              const [scores, classes] = calculateMaxScores(categories, categories_shape[1], categories_shape[2]);
+
+              // console.info("scores: " + scores);
+              // console.info("classes: " + classes);
+
+              const previousBackend = tensorflow.getBackend();
+
+              await tensorflow.setBackend("cpu");
+
+              const indexTensor = tensorflow.tidy(() => {
+                const boxes2 = tensorflow.tensor2d(geometries, [geometries_shape[1], geometries_shape[3]]);
+
+                return tensorflow.image.nonMaxSuppression(boxes2, scores, 20, 0.1, 0.1);
+              });
+
+              const indicies = indexTensor.dataSync() as Float32Array;
+
+              indexTensor.dispose();
+
+              await tensorflow.setBackend(previousBackend);
+
+              // console.info("indicies:\n" + indicies);
+
+              const r = x.shape[1];
+              const c = x.shape[2];
+
+              return buildDetectedObjects(c, r, geometries, scores, indicies, classes);
+            }
+          }
         }
       }
     };
@@ -52,14 +156,15 @@ export const CameraScreen = ({navigation}: CameraScreenProps) => {
       .catch((error) => {
         console.error(error);
       })
+      .then((detections) => {
+        if (detections) {
+          setDetections(detections);
+        }
+      })
   }, [images]);
 
   const onReady = (images: IterableIterator<tensorflow.Tensor3D>) => {
     setImages(images);
-  };
-
-  const onPress = () => {
-    setPressed(true);
   };
 
   useEffect(() => {
@@ -75,11 +180,11 @@ export const CameraScreen = ({navigation}: CameraScreenProps) => {
       const options = {
         fromTFHub: true,
         onProgress: (fraction: number) => {
-          // console.info(fraction);
+          console.info(fraction);
         }
       };
 
-      return await tensorflow.loadGraphModel(resource, options);
+      return await tensorflow.loadGraphModel(ORIGIN, options);
     };
 
     setup()
@@ -90,29 +195,6 @@ export const CameraScreen = ({navigation}: CameraScreenProps) => {
         console.error(error)
       });
   }, []);
-
-  useEffect(() => {
-    const capture = async () => {
-      if (ref.current) {
-        const options = {
-          base64: true,
-          quality: 0.5
-        };
-
-        const photo: CameraCapturedPicture = await ref.current.takePictureAsync(options);
-
-        navigation.navigate('Image', {uri: photo.uri});
-      }
-    };
-
-    capture()
-      .catch((error) => {
-        console.error(error)
-      })
-      .then(() => {
-        setPressed(false);
-      });
-  }, [pressed]);
 
   let texture: {r: number; c: number;};
   if (Platform.OS === "ios") {
@@ -133,11 +215,19 @@ export const CameraScreen = ({navigation}: CameraScreenProps) => {
           resizeHeight={224}
           resizeWidth={224}
           style={style.preview}
-          type={Camera.Constants.Type.front}
+          type={Camera.Constants.Type.back}
         />
 
         <View>
-          <TouchableOpacity onPress={onPress} style={style.capture}>
+          <Svg height='100%' viewBox={`0 0 224 224`} width='100%'>
+            <G>
+              {rectangles}
+            </G>
+          </Svg>
+        </View>
+
+        <View>
+          <TouchableOpacity onPress={() => {}} style={style.capture}>
             <Text>Snap</Text>
           </TouchableOpacity>
         </View>
