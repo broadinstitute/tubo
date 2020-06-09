bdbda6bac31cfc2c1fc567ae594f20f1342f2f58
Matt Sacks <github@mattsacks.email>
2020-06-08 22:30:21 -0700

    Add model loading screen

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -5,14 +5,20 @@ import { View } from "react-native";
 import styles from "./styles";
 import Camera from "../Camera";
 import Detections from "../Detections";
+import LoadGraph from "../LoadGraph";
 /* import Readout from "../Readout"; */
 /* import Controls from "../Controls"; */
 
 export default function App() {
   const [cameraPermission, setCameraPermission] = React.useState(false);
+  const [graph, setGraph] = React.useState<tensorflow.GraphModel>();
   const [images, setImages] = React.useState<
     IterableIterator<tensorflow.Tensor3D>
   >();
+
+  if (!graph) {
+    return <LoadGraph setGraph={setGraph} />;
+  }
 
   return (
     <View style={styles.app}>
@@ -22,7 +28,7 @@ export default function App() {
           setCameraPermission={setCameraPermission}
           setImages={setImages}
         />
-        <Detections images={images} />
+        {/* <Detections images={images} graph={graph} /> */}
       </View>
       {/* <Controls /> */}
     </View>
---
+++ src/LoadGraph/index.tsx
@@ -0,0 +1,50 @@
+import * as React from "react";
+import { Text, View } from "react-native";
+import * as tensorflow from "@tensorflow/tfjs";
+import styles from "./styles";
+
+// TODO: Make this an env variable
+const ORIGIN =
+  "https://tfhub.dev/tensorflow/tfjs-model/ssd_mobilenet_v2/1/default/1";
+
+interface Props {
+  setGraph: (graph: tensorflow.GraphModel) => void;
+}
+
+export default function LoadGraph(props: Props): JSX.Element {
+  const { setGraph } = props;
+  const [progress, setProgress] = React.useState(0);
+
+  React.useEffect(() => {
+    const loadGraph = async () => {
+      await tensorflow.ready();
+
+      const options = {
+        fromTFHub: true,
+        onProgress: (loading: number) => {
+          setProgress(Math.round(loading * 100));
+        }
+      };
+
+      const graph = await tensorflow.loadGraphModel(ORIGIN, options);
+
+      setGraph(graph);
+    };
+
+    loadGraph().catch((error) => {
+      console.info(`Error!: ${error}`);
+    });
+  }, []);
+
+  return (
+    <View style={styles.loading}>
+      <Text style={styles.loadingHeader}>Loading model…</Text>
+      <View style={styles.progressContainer}>
+        <View style={styles.progressBar}>
+          <View style={{ ...styles.progress, width: `${progress}%` }} />
+        </View>
+        <Text>{progress}%</Text>
+      </View>
+    </View>
+  );
+}
