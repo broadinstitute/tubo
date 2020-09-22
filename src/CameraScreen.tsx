47c64a30dab06e5fc1111d57b095dd88c64e652a
Allen Goodman <allen.goodman@icloud.com>
2020-09-21 23:39:18 -0400

    inlined styles

--- src/CameraScreen.tsx
+++ src/CameraScreen.tsx
@@ -2,10 +2,9 @@ import * as tensorflow from "@tensorflow/tfjs";
 import React, { useEffect, useState } from "react";
 import Svg, { G, Rect, Text as TextSVG } from "react-native-svg";
 import { Camera } from "expo-camera";
-import { Text, Platform, View, LayoutRectangle } from "react-native";
+import { Text, Platform, View, LayoutRectangle, StyleSheet } from "react-native";
 import { askAsync, CAMERA } from "expo-permissions";
 import { cameraWithTensors } from "@tensorflow/tfjs-react-native";
-import { cameraScreenStyle } from "./CameraScreen.style";
 
 const C = 152;
 const R = 200;
@@ -16,6 +15,54 @@ const ORIGIN =
 const TensorCamera = cameraWithTensors(Camera);
 
 type BoundingBox = [number, number, number, number];
+
+const cameraScreenStyle = StyleSheet.create({
+  app: {
+    backgroundColor: "#fff",
+    display: "flex",
+    flexDirection: "column",
+    height: "100%",
+    width: "100%"
+  },
+  capture: {
+    alignSelf: "center",
+    backgroundColor: "#fff",
+    borderRadius: 5,
+    flex: 0,
+    paddingHorizontal: 20
+  },
+  camera: {
+    bottom: 0,
+    left: 0,
+    position: "absolute",
+    right: 0,
+    top: 0,
+    zIndex: 1
+  },
+  cameraContainer: {
+    display: "flex",
+    height: "50%",
+    width: "100%"
+  },
+  controls: {
+    padding: 24
+  },
+  header: {
+    color: "black",
+    fontSize: 42,
+    fontWeight: "900"
+  },
+  preview: {
+    alignItems: "center",
+    flex: 1,
+    justifyContent: "flex-end"
+  },
+  predictions: {
+    height: "100%",
+    width: "100%",
+    zIndex: 19
+  }
+});
 
 const calculateMaxScores = (
   scores: Float32Array,
